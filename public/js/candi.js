07eff305a257d8301186ce8f1387ccad297156c9
bestboom <certseek@gmail.com>
2019-01-15 22:57:57 +0500

    Update candi.js

--- public/js/candi.js
+++ public/js/candi.js
@@ -12,9 +12,9 @@ const tronWeb = new TronWeb(
     eventServer,
     privateKey
 );
-
+init:async function(){
 let contractInstance = await tronWeb.contract().at("TJHfHUk2YxXXiAspfx2kfAPZaAQasSmaZ2");
-
+},
 userTotalPixels:async function(){
         console.log(await this.contractInstance.userTotalPixels(tronWeb.defaultAddress.hex).call());
 }, 
