f0253fd98dd2788f1dddc92b8a96e92852de2ac5
bestboom <certseek@gmail.com>
2019-01-15 22:42:46 +0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,23 @@
 $(document).ready(() => {
+  
+  window.onload = function() {
+  if (!window.tronWeb) {
+
+const HttpProvider = TronWeb.providers.HttpProvider;
+const fullNode = new HttpProvider('https://api.shasta.trongrid.io');
+const solidityNode = new HttpProvider('https://api.shasta.trongrid.io');
+const eventServer = 'https://api.shasta.trongrid.io/';
+    
+    const tronWeb = new TronWeb(
+        fullNode,
+        solidityNode,
+        eventServer,
+    );
+
+    window.tronWeb = tronWeb;
+  }
+};
+ 
 
 $("#claim").click(async function(event) {
   	event.preventDefault();
