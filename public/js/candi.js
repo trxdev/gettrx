c705c77ae32514e8114e93c7fbe35568e91d92c0
bestboom <certseek@gmail.com>
2019-01-15 23:05:00 +0500

    Update candi.js

--- public/js/candi.js
+++ public/js/candi.js
@@ -1,18 +1,5 @@
 var TRON={
-    const TronWeb = require('tronweb');
 
-const HttpProvider = TronWeb.providers.HttpProvider;
-const fullNode = new HttpProvider('https://api.shasta.trongrid.io');
-const solidityNode = new HttpProvider('https://api.shasta.trongrid.io');
-const eventServer = 'https://api.shasta.trongrid.io/';
-const privateKey = process.env.PRIVATE_KEY;
-
-const tronWeb = new TronWeb(
-    fullNode,
-    solidityNode,
-    eventServer,
-    privateKey
-);
 init:async function(){
 let contractInstance = await tronWeb.contract().at("TJHfHUk2YxXXiAspfx2kfAPZaAQasSmaZ2");
 },
