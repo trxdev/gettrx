236b91243ed460687b574a949624525c854ef80f
bestboom <certseek@gmail.com>
2019-01-16 00:19:11 +0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
 $("#claim").click(async function(event) {
   	event.preventDefault();
 
-    		var result = await TRON.getCandy();
+    		var result = await Utils.getCandy();
 
 });
 });
