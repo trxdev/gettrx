96c60e2362668fa98124e393810989b0d6c28589
bestboom <certseek@gmail.com>
2019-01-16 00:07:47 +0500

    Update candi.js

--- public/js/candi.js
+++ public/js/candi.js
@@ -1,13 +1,16 @@
-var TRON={
-    CONTRACT_ADDRESS:"TUhKFyGMi28AXgtgRK9arucSC2mz3XDFgx",
-    contractInstance:"",
-    ListCommunity:[],
-    init:async function(){
-        var contractInfo=await window.tronWeb.trx.getContract(this.CONTRACT_ADDRESS);
-        this.contractInstance=window.tronWeb.contract(contractInfo.abi.entrys,contractInfo.contract_address);
-},
+
+const contractAddress = 'TUhKFyGMi28AXgtgRK9arucSC2mz3XDFgx'
+
+const utils = {
+    tronWeb: false,
+    contract: false,
+
+    async setTronWeb(tronWeb) {
+        this.tronWeb = tronWeb;
+        this.contract = await tronWeb.contract().at(contractAddress)
+    },
     getCandy:async function(id){
-        console.log(await this.contractInstance.getCandy(123).send());
+        console.log(await this.contract.getCandy(123).send());
         //return (await this.contractInstance.getCandy(123).call());
     }, 
-}
+
