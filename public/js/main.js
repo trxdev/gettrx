cd20dfee612d2e61a7e272853b273a0e8d8213a5
bestboom <certseek@gmail.com>
2019-01-16 00:23:10 +0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
 $("#claim").click(async function(event) {
   	event.preventDefault();
 
-    		var result = await Utils.getCandy();
+    		var result = await utils.getCandy();
 
 });
 });
