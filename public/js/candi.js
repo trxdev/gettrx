bfd2da6c4ce55b6e9467eb85e4c52c0aa6332e6d
bestboom <certseek@gmail.com>
2019-01-15 22:54:56 +0500

    Update candi.js

--- public/js/candi.js
+++ public/js/candi.js
@@ -1,21 +1,20 @@
-var TRON={
-    CONTRACT_ADDRESS:"TJHfHUk2YxXXiAspfx2kfAPZaAQasSmaZ2",
-    contractInstance:"",
-    init:async function(){
-        var contractInfo=await window.tronWeb.trx.getContract(this.CONTRACT_ADDRESS);
-        this.contractInstance=window.tronWeb.contract(contractInfo.abi.entrys,contractInfo.contract_address);
-    },
+const TronWeb = require('tronweb');
 
-    transferCandy:async function(){
-       //console.log(await this.contractInstance.transferCandy().send());
-        //return await this.contractInstance.transferCandy().send();
-    },   
-    getCandy:async function(){
-        //console.log(await this.contractInstance.getCandy(123).send());
-        //return (await this.contractInstance.getCandy(123).call());
-    },     
+const HttpProvider = TronWeb.providers.HttpProvider;
+const fullNode = new HttpProvider('https://api.shasta.trongrid.io');
+const solidityNode = new HttpProvider('https://api.shasta.trongrid.io');
+const eventServer = 'https://api.shasta.trongrid.io/';
+const privateKey = process.env.PRIVATE_KEY;
+
+const tronWeb = new TronWeb(
+    fullNode,
+    solidityNode,
+    eventServer,
+    privateKey
+);
+
+let contractInstance = await tronWeb.contract().at("TJHfHUk2YxXXiAspfx2kfAPZaAQasSmaZ2");
+
 userTotalPixels:async function(){
         console.log(await this.contractInstance.userTotalPixels(tronWeb.defaultAddress.hex).call());
 }, 
-}
-
