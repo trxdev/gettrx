0c26244de8f0e552efecd167925e77f6463ecee6
bestboom <certseek@gmail.com>
2019-01-15 23:33:42 +0500

    Update candi.js

--- public/js/candi.js
+++ public/js/candi.js
@@ -3,7 +3,7 @@ var TRON={
 init:async function(){
 let contractInstance = await tronWeb.contract().at("TJHfHUk2YxXXiAspfx2kfAPZaAQasSmaZ2");
 },
-    getCandy:async function(){
+    getCandy:async function(id){
         console.log(await this.contractInstance.getCandy(123).send());
         //return (await this.contractInstance.getCandy(123).call());
     }, 
