723a5519c04671d50ddb7ff81fc48bf1cca3d6e6
bestboom <certseek@gmail.com>
2019-01-15 22:35:08 +0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,5 @@
 $(document).ready(() => {
-    let intervalID = setInterval(function () {
-    if (typeof window.tronWeb == 'object') {
-      window.tronWeb.on("addressChanged", showAccountInfo)
-      clearInterval(intervalID);
-    }
-}, 10)
+
 $("#claim").click(async function(event) {
   	event.preventDefault();
 
