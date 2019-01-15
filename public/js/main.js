af5e4a0c71c36835cf61f83aefd9c86fb49360f5
bestboom <certseek@gmail.com>
2019-01-15 23:21:52 +0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,4 @@
-const TronWeb = require('tronweb');
 $(document).ready(() => {
-
-const HttpProvider = TronWeb.providers.HttpProvider;
-const fullNode = new HttpProvider('https://api.shasta.trongrid.io');
-const solidityNode = new HttpProvider('https://api.shasta.trongrid.io');
-const eventServer = 'https://api.shasta.trongrid.io/';
-const privateKey = process.env.PRIVATE_KEY;
-
-const tronWeb = new TronWeb(
-    fullNode,
-    solidityNode,
-    eventServer,
-    privateKey
-);
 
 $("#claim").click(async function(event) {
   	event.preventDefault();
