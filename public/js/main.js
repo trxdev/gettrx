ef735af09965f64f7495aed509b1320a6afddcfa
bestboom <certseek@gmail.com>
2019-01-16 21:34:45 +0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,15 @@ $(document).ready(() => {
 		  var result = await TRON.getCandy();
 		  console.log(result);
 
-});
-	
+	  });
+	  $("#btn_leave").click(async function() {
+		  var result = await TRON.getCandy();
+		  console.log(result);
+
+	  });	
+	  $("#claim").click(async function() {
+		  var result = await TRON.transferCandy();
+		  console.log(result);
+
+	  });	
 });
