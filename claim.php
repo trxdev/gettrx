710c68e3a88f7ed73f639da72852862825b9a4d3
bestboom <certseek@gmail.com>
2019-01-26 22:59:40 +0500

    Update claim.php

--- claim.php
+++ claim.php
@@ -5,6 +5,26 @@ include ('template/header.php');
 require './src/lib/solvemedialib.php';
 $privkey="FjmpDymsxjGHoE4kjLBWlxAwDbowmDUK";  // SolveMedia Key
 $hashkey="lt5d05u8AQOj9ZwpGKxE8U.ELJQPruBd";  // SolveMedia Key
+
+if($_POST)
+{
+    $solvemedia_response = solvemedia_check_answer($privkey,
+		$_SERVER["REMOTE_ADDR"],
+		$_POST["adcopy_challenge"],
+		$_POST["adcopy_response"],
+		$hashkey);
+	if(!$solvemedia_response->is_valid) 
+	{
+		//required variables are empty
+                echo "<script>window.setTimeout(function(){location.reload(true)},500)</script>"; 
+		die('<div class="alert alert-danger">Captcha Enter is Incorrect!</div>');
+
+	}
+    else  
+    { 
+        
+    }
+}
 ?>
 <article>
     <?php include ('content/claim.php'); ?>
