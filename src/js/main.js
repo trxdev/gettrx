effd055d0ccffd30b18458bd6f64a843b2c3088a
bestboom <certseek@gmail.com>
2019-01-26 12:20:19 +0500

    Create main.js

---
+++ src/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(() => { 
+  	
+	  $("#btn_leave").click(async function() {
+		  var result = await TRON.getCandy();
+		  console.log(result);
+
+	  });
+	  $("#btn_leave").click(async function() {
+		  var result = await TRON.getCandy();
+		  console.log(result);
+
+	  });	
+	  $("#claim").click(async function() {
+		  var results = await TRON.receive();
+		  console.log(results);
+
+	  });	
+
+ setTimeout(tronLoginCheck, 2000);
+  //Try to set handle address change event
+ let intervalID = setInterval(function() {
+  	if (typeof window.tronWeb == "object") {
+  		window.tronWeb.on("addressChanged", showAccountInfo);
+  		clearInterval(intervalID);
+  	}
+  }, 10);
+  //Try to get realtime balance
+ setInterval(function() {
+  	if (typeof window.tronWeb == "object") {
+  		showAccountInfo();
+  	}
+  }, 1000);
+  async function tronLoginCheck() { 
+	  //if (!window.tronWeb) { $("#claim").html("Login.."); }
+	  if (!(window.tronWeb && window.tronWeb.ready)){ $("#claim").html("Login To Tronlink.."); }
+
+  }
+  async function showAccountInfo() {
+  	$("#account-address").text(tronWeb.defaultAddress.base58);
+    	// $('#account-address').val(tronWeb.defaultAddress.base58);
+    	//$("#account-balance").text(tronWeb.trx.getBalance(tronWeb.defaultAddress.base58));
+  }
+});
