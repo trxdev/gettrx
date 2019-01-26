0c2ee77aa55ac1f3aa8debe85bc2e7eb6c02fdb8
bestboom <certseek@gmail.com>
2019-01-26 16:15:19 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
  echo 'bad';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require('/src/lib/solvemedialib.php');
 
 $privkey="FjmpDymsxjGHoE4kjLBWlxAwDbowmDUK";  // SolveMedia Key
