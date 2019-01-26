25dd7d87f3f2e03b7f9a7f45b26e66629655b0e5
bestboom <certseek@gmail.com>
2019-01-27 00:12:50 +0500

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,9 @@ $(document).ready(() => {
     { 
         g.preventDefault();
         $('#verify').attr('disabled', ''); // disable upload button
-        $('#human_verify').modal('hide');
+        //$('#human_verify').modal('hide');
         
-        $(this).ajaxSubmit({
+        $('#logsubmit').ajaxSubmit({
         target: '#output-msg',        
         success:  afterSuccess_log //call function after success
         });
