9cf01a4fe5a5529e5c1e0cff5b687c088041fda7
bestboom <certseek@gmail.com>
2019-01-26 21:49:42 +0500

    Update claim.php

--- content/claim.php
+++ content/claim.php
@@ -4,7 +4,7 @@
         <div class="col-md-12" style="padding:45px;border: 1px solid #000;border-radius: 7px;background: #e9ecef;">
           
           <h6 class="incenter">CLAIM FREE TRX NOW</h6>
-          <div class="incenter" style="margin:15px;"><a class="button button-sm button-secondary button-nina"><span id="claim">CLAIM</span></a></div>
+          <div class="incenter" style="margin:15px;"></div>
           <h5 class="incenter">BECOME PREMIUM MEMBER TO CLAIM MORE</h5><br>
           <div class="incenter"><iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=D2SUKIIVOLPOL" style="overflow:hidden;width:300px;height:250px;" frameborder="0"></iframe></div>
         </div>
@@ -15,7 +15,7 @@
           <div><iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=TX5FVE00GYSPK" style="overflow:hidden;width:468px;height:60px;" frameborder="0"></iframe></div>
           <h2 style="font-weight: 500;text-transform: uppercase;color: #29293a;margin-bottom: 0;letter-spacing: .02em;line-height: 1.375;text-align: left!important;">Tron Faucet To Claim TRX</h2>
           <div class="divider"></div>
-          <p style="margin-top: 40px;letter-spacing: .02em;text-align: left!important;">Prove you are human being!</p>
+          <p style="margin-top: 40px;letter-spacing: .02em;text-align: left!important;">Prove you are human being!<br/><a class="button button-sm button-secondary button-nina"><span id="human">Prove To Be Human</span></a></p>
         </div>
       </div>
       </div>
