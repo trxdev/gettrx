e636235659347ddf4cb2a390ead662c1bd926841
bestboom <certseek@gmail.com>
2019-01-26 21:40:40 +0500

    Update claim.php

--- content/claim.php
+++ content/claim.php
@@ -1,9 +1,27 @@
-require('./src/lib/solvemedialib.php');
-
-$privkey="FjmpDymsxjGHoE4kjLBWlxAwDbowmDUK";  // SolveMedia Key
-$hashkey="lt5d05u8AQOj9ZwpGKxE8U.ELJQPruBd";  // SolveMedia Key
-
-
-
-<center><?php echo solvemedia_get_html("xLxIw6ibOZWL52J97WjID7wkCMJzave8"); ?> </center><br>
-<p>Not a valid claim session</p>
+<div class="container container-bigger" style="padding-top: 110px;">
+    <div class="row">
+      <div class="col-md-5">
+        <div class="col-md-12" style="padding:45px;border: 1px solid #000;border-radius: 7px;background: #e9ecef;">
+          
+          <h6 class="incenter">CLAIM FREE TRX NOW</h6>
+          <div class="incenter" style="margin:15px;"><a class="button button-sm button-secondary button-nina"><span id="claim">CLAIM</span></a></div>
+          <h5 class="incenter">BECOME PREMIUM MEMBER TO CLAIM MORE</h5><br>
+          <div class="incenter"><iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=D2SUKIIVOLPOL" style="overflow:hidden;width:300px;height:250px;" frameborder="0"></iframe></div>
+        </div>
+      </div>
+      <div class="col-md-7">
+        <div class="col-md-12" style="padding:35px;border: 1px solid #000;border-radius: 7px;background: #e9ecef;">
+          <h4 style="font-weight: 300;line-height: 1.41667;text-transform: none;color: #29293a;text-align: left!important;">GET FREE TRX :: <span id="account-address"></span> : <span id="account-balance"></span></h4>
+          <div><iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=TX5FVE00GYSPK" style="overflow:hidden;width:468px;height:60px;" frameborder="0"></iframe></div>
+          <h2 style="font-weight: 500;text-transform: uppercase;color: #29293a;margin-bottom: 0;letter-spacing: .02em;line-height: 1.375;text-align: left!important;">Tron Faucet To Claim TRX</h2>
+          <div class="divider"></div>
+          <p style="margin-top: 40px;letter-spacing: .02em;text-align: left!important;">Prove you are human being!</p>
+        </div>
+      </div>
+      </div>
+     <div class="incenter" style="margin-top:15px;">
+       <iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=LOQBU2V7573MQ" style="overflow:hidden;width:728px;height:90px;" frameborder="0"></iframe>
+     </div>
+    
+    
+  </div>
