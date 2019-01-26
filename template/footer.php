4b414cf4ee49af5bc85dbc56ea361b04942e5996
bestboom <certseek@gmail.com>
2019-01-26 21:35:46 +0500

    Create footer.php

---
+++ template/footer.php
@@ -0,0 +1,8 @@
+    <script src="/src/js/candi.js"></script>  
+        <script src="/src/js/main.js"></script>
+    <footer class="foot">
+        <small>&copy;<?php echo date('Y'); ?> :: GET TRX</small>
+    </footer>
+ </body>
+</html>   
+         
