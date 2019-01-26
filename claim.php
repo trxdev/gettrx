c93eba2bff48c6cc334ff3b55eb9258fe528c85f
bestboom <certseek@gmail.com>
2019-01-26 14:27:54 +0500

    Create claim.php

---
+++ claim.php
@@ -0,0 +1,6 @@
+<?php
+require('/src/lib/solvemedialib.php');
+$privkey="FjmpDymsxjGHoE4kjLBWlxAwDbowmDUK";  // SolveMedia Key
+$hashkey="lt5d05u8AQOj9ZwpGKxE8U.ELJQPruBd";  // SolveMedia Key
+?>
+<center><?php echo solvemedia_get_html("xLxIw6ibOZWL52J97WjID7wkCMJzave8"); ?> </center><br>
