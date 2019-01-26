80f63c832a8746c64dbd75fc3d2f8f95cb6b7315
bestboom <certseek@gmail.com>
2019-01-26 23:42:27 +0500

    Update claim.php

--- content/claim.php
+++ content/claim.php
@@ -23,7 +23,7 @@
        <iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=LOQBU2V7573MQ" style="overflow:hidden;width:728px;height:90px;" frameborder="0"></iframe>
      </div>
     
-    
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.js"></script>
          <div class="modal fade" id="human_verify" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
