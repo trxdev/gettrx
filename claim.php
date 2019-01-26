e186780252b72742b672dc8018adce636d887999
bestboom <certseek@gmail.com>
2019-01-26 14:40:10 +0500

    Update claim.php

--- claim.php
+++ claim.php
@@ -2,7 +2,5 @@
 // Comment these lines to hide errors
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require 'includes/config.php';
-require 'includes/functions.php';
 require 'includes/claim.php';
 init();
