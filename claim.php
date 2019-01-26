9b09ff38cd5e7998d3d5d0e9017e454b3ccdf5b3
bestboom <certseek@gmail.com>
2019-01-26 22:25:41 +0500

    Update claim.php

--- claim.php
+++ claim.php
@@ -2,62 +2,8 @@
 session_start();
 error_reporting(E_ALL);
 include ('template/header.php');
-require 'src/lib/solvemedialib.php';
-
-
-
-$privkey="FjmpDymsxjGHoE4kjLBWlxAwDbowmDUK";  // SolveMedia Key
-$hashkey="lt5d05u8AQOj9ZwpGKxE8U.ELJQPruBd";  // SolveMedia Key
-
 ?>
 <article>
-<div class="container container-bigger" style="padding-top: 110px;">
-    <div class="row">
-      <div class="col-md-5">
-        <div class="col-md-12" style="padding:45px;border: 1px solid #000;border-radius: 7px;background: #e9ecef;">
-          
-          <h6 class="incenter">CLAIM FREE TRX NOW</h6>
-          <div class="incenter" style="margin:15px;"></div>
-          <h5 class="incenter">BECOME PREMIUM MEMBER TO CLAIM MORE</h5><br>
-          <div class="incenter"><iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=D2SUKIIVOLPOL" style="overflow:hidden;width:300px;height:250px;" frameborder="0"></iframe></div>
-        </div>
-      </div>
-      <div class="col-md-7">
-        <div class="col-md-12" style="padding:35px;border: 1px solid #000;border-radius: 7px;background: #e9ecef;">
-          <h4 style="font-weight: 300;line-height: 1.41667;text-transform: none;color: #29293a;text-align: left!important;">GET FREE TRX :: <span id="account-address"></span> : <span id="account-balance"></span></h4>
-          <div><iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=TX5FVE00GYSPK" style="overflow:hidden;width:468px;height:60px;" frameborder="0"></iframe></div>
-          <h2 style="font-weight: 500;text-transform: uppercase;color: #29293a;margin-bottom: 0;letter-spacing: .02em;line-height: 1.375;text-align: left!important;">Tron Faucet To Claim TRX</h2>
-          <div class="divider"></div>
-          <p style="margin-top: 40px;letter-spacing: .02em;text-align: left!important;">Prove you are human being!<br/><a class="button button-sm button-secondary button-nina" data-toggle="modal" data-target="#human_verify"><span id="human">Prove To Be Human</span></a></p>
-        </div>
-      </div>
-      </div>
-     <div class="incenter" style="margin-top:15px;">
-       <iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=LOQBU2V7573MQ" style="overflow:hidden;width:728px;height:90px;" frameborder="0"></iframe>
-     </div>
-    
-    
-         <div class="modal fade" id="human_verify" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
-            <div class="modal-dialog modal-sm" role="document">
-                <div class="modal-content small-modal">
-                  <div class="modal-header">
-                    <h5 class="modal-title">Prove To Be Human</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div class="modal-new" style="padding: 60px">
-                    <form>
-                      <div class="form-group">
-                        <center><?php echo solvemedia_get_html("xLxIw6ibOZWL52J97WjID7wkCMJzave8"); ?> </center><br>
-                      </div>
-                      <button id="pay" type="submit" class="btn btn-primary">CLAIM</button>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-    
-  </div>
+    <?php include ('content/claim.php'); ?>
 </article>
 <?php include ('template/footer.php'); ?>
