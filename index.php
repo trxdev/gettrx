2f37e18f9590c8f1b1c619bb1d25f2c81317d2ea
bestboom <certseek@gmail.com>
2019-01-26 16:50:02 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
+include ('template/header.php');
+require 'src/lib/solvemedialib.php';
  echo 'bad';
 error_reporting(E_ALL);
-require('src/lib/solvemedialib.php');
+
 
 $privkey="FjmpDymsxjGHoE4kjLBWlxAwDbowmDUK";  // SolveMedia Key
 $hashkey="lt5d05u8AQOj9ZwpGKxE8U.ELJQPruBd";  // SolveMedia Key
@@ -19,6 +21,6 @@ $solvemedia_response = solvemedia_check_answer($privkey,
 <center><?php echo solvemedia_get_html("xLxIw6ibOZWL52J97WjID7wkCMJzave8"); ?> </center><br>
 <p>Not a valid claim session</p>
 <?php
-include ('template/header.php');
+
 ?>
 <h1>good</h1>
