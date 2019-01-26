ba636ec84fba22094126efd45dca310d72cc1775
bestboom <certseek@gmail.com>
2019-01-26 16:19:55 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  echo 'bad';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
-include ('/template/header.php');
+require 'includes/config.php';
+include ('template/header.php');
 ?>
 <h1>good</h1>
