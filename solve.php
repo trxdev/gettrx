1825aa3aa697c7b8fbee6f2366a4b99517018a33
bestboom <certseek@gmail.com>
2019-01-27 21:20:59 +0500

    Update solve.php

--- solve.php
+++ solve.php
@@ -20,12 +20,12 @@ if($_POST)
 	}
     else  
     { 
-	    echo '<script>let mint = (async function(){
+	    echo '<script>
 	    var check = await TRON.myLastReceive(tronWeb.defaultAddress.base58);
 	    console.log(check);
 	    if (check == true) {
 		    $("#verify").html("can receive");
-	    } else { $("#verify").html("can not receive"); } })();</script>';
+	    } else { $("#verify").html("can not receive"); }</script>';
      //echo "<script>$('#human_verify').modal('hide');</script>"; 
      //echo "<script>let mint = (async function(){var results = await TRON.receive();})();</script>"; 	    
     }
