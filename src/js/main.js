5c69021aed550cb88c6627816daf979131b89953
bestboom <certseek@gmail.com>
2019-01-27 14:15:42 +0500

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,18 @@
 $(document).ready(() => { 
-	var options = { target: '#output-msg' }; 
     $('#logsubmit').on('submit', function(g)
     { 
         g.preventDefault();
         $('#verify').attr('disabled', ''); // disable upload button
         $('#human_verify').modal('hide');
         
-        $(this).ajaxSubmit(options);
+        $(this).ajaxSubmit({
+	//target: '#output-msg',  	
+	
+              
+        success:  function(responseText){
+		$('#output-msg').html(responseText);//call function after success
+	}
+        });
 	 return false;
     });	
 	function afterSuccess_log()
