dfb7276ba38aa440f9a513b5fe1fcb9700263a68
bestboom <certseek@gmail.com>
2019-01-27 19:06:17 +0500

    Update solve.php

--- solve.php
+++ solve.php
@@ -21,7 +21,7 @@ if($_POST)
     else  
     { 
      echo "<script>$('#human_verify').modal('hide');</script>"; 
-     echo "<script>var results = await TRON.receive();</script>"; 	    
+     echo "<script>async function(){var results = await TRON.receive();}</script>"; 	    
     }
 }
 ?>
