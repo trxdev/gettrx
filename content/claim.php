f0d80940f67acfa68f4f103a0ba7692a745a8df3
bestboom <certseek@gmail.com>
2019-01-27 10:55:51 +0500

    Update claim.php

--- content/claim.php
+++ content/claim.php
@@ -35,7 +35,7 @@
                     </button>
                   </div>
                   <div class="modal-new" style="padding: 60px">
-                    <form action="" id="logsubmit">
+                    <form action="" method="post" id="logsubmit">
                       <div class="form-group">
                         <center><?php echo solvemedia_get_html("xLxIw6ibOZWL52J97WjID7wkCMJzave8"); ?> </center><br>
                       </div>
