083a0751d4911892da9de9051867cc8dbf69542e
bestboom <certseek@gmail.com>
2019-01-29 13:50:08 +0500

    Update home.php

--- content/home.php
+++ content/home.php
@@ -1,25 +1,29 @@
 <div class="container container-bigger" style="padding-top: 110px;">
     <div class="row">
-      <div class="col-md-5">
-        <div class="col-md-12" style="padding:45px;border: 1px solid #000;border-radius: 7px;background: #e9ecef;">
-          
-          <h6 class="incenter">CLAIM FREE TRX NOW</h6>
-          <div class="incenter" style="margin:15px;"><a class="button button-sm button-secondary button-nina"><span id="claim">CLAIM</span></a></div>
-          <h5 class="incenter">BECOME PREMIUM MEMBER TO CLAIM MORE</h5><br>
-          <div class="incenter"><iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=D2SUKIIVOLPOL" style="overflow:hidden;width:300px;height:250px;" frameborder="0"></iframe></div>
+        <div class="col-md-5">
+            <div class="col-md-12" style="padding:45px;border: 1px solid #000;border-radius: 7px;background: #e9ecef;">
+
+                <h6 class="incenter">CLAIM FREE TRX NOW</h6>
+                <div class="incenter" style="margin:15px;"><a class="button button-sm button-secondary button-nina"><span id="claim">CLAIM</span></a></div>
+                <h5 class="incenter">BECOME PREMIUM MEMBER TO CLAIM MORE</h5>
+                <br>
+                <div class="incenter">
+                    <iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=D2SUKIIVOLPOL" style="overflow:hidden;width:300px;height:250px;" frameborder="0"></iframe>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="col-md-7">
-        <div class="col-md-12" style="padding:35px;border: 1px solid #000;border-radius: 7px;background: #e9ecef;">
-            <div><?php include('template/info.php'); ?></div>
-          <p style="margin-top: 40px;letter-spacing: .02em;text-align: left!important;">Tron is worls no.  cryptocurrency with a market cap of  . In last 24 hours tron coin volume remains with tron price right now trading at.<br>
-You can claim free ton on this tron faucet. We give away free 10,000 SUN (0.01 TRX) every 10 minutes. To claim free tron, you must follow rules and guidelines and should not do any activities that harm the urpose fo this faucet.</p>
+        <div class="col-md-7">
+            <div class="col-md-12" style="padding:35px;border: 1px solid #000;border-radius: 7px;background: #e9ecef;">
+                <div>
+                    <?php include('template/info.php'); ?>
+                </div>
+                <p style="margin-top: 40px;letter-spacing: .02em;text-align: left!important;">Tron is world no. cryptocurrency with a market cap of . In last 24 hours tron coin volume remains with tron price right now trading at.
+                    <br> You can claim free trons on this tron faucet. We give away 10,000 SUN (0.01 TRX) every 10 minutes. To claim free tron, you must follow rules and guidelines and should not do any activities that harm the good purpose fo this faucet.</p>
+            </div>
         </div>
-      </div>
-      </div>
-     <div class="incenter" style="margin-top:15px;">
-       <iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=LOQBU2V7573MQ" style="overflow:hidden;width:728px;height:90px;" frameborder="0"></iframe>
-     </div>
-    
-    
-  </div>
+    </div>
+    <div class="incenter" style="margin-top:15px;">
+        <iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=LOQBU2V7573MQ" style="overflow:hidden;width:728px;height:90px;" frameborder="0"></iframe>
+    </div>
+
+</div>
