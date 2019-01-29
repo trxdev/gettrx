d19a65e8b60e3d6d37ce58e84942bea64ba5d328
bestboom <certseek@gmail.com>
2019-01-29 13:10:07 +0500

    Update claim.php

--- content/claim.php
+++ content/claim.php
@@ -37,33 +37,16 @@
     <div class="modal fade" id="human_verify" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Prove To Be Human</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-new" style="padding: 60px">
-                    <div id="output-msg"></div>
-                    <form action="solve.php" method="post" id="logsubmit">
-                        <div class="form-group">
-                            <span id="capt"><center><?php echo solvemedia_get_html("xLxIw6ibOZWL52J97WjID7wkCMJzave8", null, true); ?></center></span>
-                            <br>
-                        </div>
-                        <center>
-                            <button id="verify" type="submit" class="button button-sm button-secondary button-nina">CLAIM</button>
-                        </center>
-                    </form>
-                </div>
+                <?php include('template/modals/verification.php'); ?>
             </div>
         </div>
     </div>
-<div class="modal fade" id="premium_modal" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+    <div class="modal fade" id="premium_modal" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
         <div class="modal-dialog" role="document">
-    <div class="modal-content">
-        <?php include('template/modals/premium.php'); ?>
+            <div class="modal-content">
+                <?php include('template/modals/premium.php'); ?>
             </div>
-            </div></div>
-    
+        </div>
+    </div>
 
 </div>
