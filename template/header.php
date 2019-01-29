e7abdee66245fa0c1450278ea49d6b008f7e1f45
bestboom <certseek@gmail.com>
2019-01-29 13:18:44 +0500

    Update header.php

--- template/header.php
+++ template/header.php
@@ -9,18 +9,16 @@
     <link rel="stylesheet" href="/template/style.css">
     <link rel='favicon icon' type='image/x-icon' href='/images/favicon.ico' />
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>    
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
 <body>
 <div class="wrap">
-
     <header>
         <div class="header">
         <div class="left" style="justify-content: flex-start;">
         </div>
-        <div class="center"><h1 class="text-center"><a href="/index.html" style="color:#fff;text-decoration:none;">Get TRX</a></h1></div>
+        <div class="center"><h1 class="text-center"><a href="/" style="color:#fff;text-decoration:none;">Get TRX</a></h1></div>
         <div class="right" style="justify-content: flex-end;">
         </div> 
     </div>
