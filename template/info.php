68eb19a23cb17376d43acbe43457b4d83116af67
bestboom <certseek@gmail.com>
2019-01-29 13:47:18 +0500

    Update info.php

--- template/info.php
+++ template/info.php
@@ -3,4 +3,5 @@
                     <iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=TX5FVE00GYSPK" style="overflow:hidden;width:468px;height:60px;" frameborder="0"></iframe>
                 </div>
                 <h2 style="font-weight: 500;text-transform: uppercase;color: #29293a;margin-bottom: 0;letter-spacing: .02em;line-height: 1.375;text-align: left!important;">Tron Faucet To Claim FREE TRON</h2>
+<div class="divider"></div>
                 
