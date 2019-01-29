f03d21eaa77dbfd53124ab769ae922f080703eea
bestboom <certseek@gmail.com>
2019-01-29 14:59:52 +0500

    Update candi.js

--- src/js/candi.js
+++ src/js/candi.js
@@ -1,26 +1,26 @@
-var TRON={
-    CONTRACT_ADDRESS:"TW7gzeUB5L4m11cYdeZLTyd6FT4huMcSeK",
-    contractInstance:"",
-    init:async function(){
-        var contractInfo=await window.tronWeb.trx.getContract(this.CONTRACT_ADDRESS);
-        this.contractInstance=window.tronWeb.contract(contractInfo.abi.entrys,contractInfo.contract_address);
+var TRON = {
+    CONTRACT_ADDRESS: "TJuGjPrzUZNJHL9seB5JgbAmdAi47Aqeps",
+    contractInstance: "",
+    init: async function() {
+        var contractInfo = await window.tronWeb.trx.getContract(this.CONTRACT_ADDRESS);
+        this.contractInstance = window.tronWeb.contract(contractInfo.abi.entrys, contractInfo.contract_address);
     },
 
-    receive:async function(){
-       return await this.contractInstance.receive().send();
+    receive: async function() {
+        return await this.contractInstance.receive().send();
         console.log(await this.contractInstance.receive().send());
     },
-    canReceive:async function(address){
+    canReceive: async function(address) {
         return (await this.contractInstance.canReceive(tronWeb.defaultAddress.base58).call());
     },
 }
-var timeOutID=setTimeout(tryInstall,100)
-function tryInstall(){
-    if(window.tronWeb&&window.tronWeb && window.tronWeb.ready){
+	var timeOutID = setTimeout(tryInstall, 100)
+
+function tryInstall() {
+    if (window.tronWeb && window.tronWeb && window.tronWeb.ready) {
         clearTimeout(timeOutID);
         TRON.init();
-    }
-    else{
-        timeOutID=setTimeout(tryInstall,100)
+    } else {
+        timeOutID = setTimeout(tryInstall, 100)
     }
 }
