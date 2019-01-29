6898ea19584e2f727c78d4536cc4fa61cbc29583
bestboom <certseek@gmail.com>
2019-01-29 12:48:16 +0500

    Update premium.php

--- template/modals/premium.php
+++ template/modals/premium.php
@@ -1,6 +1,4 @@
-<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-    <div class="modal-content">
+
         <div class="modal-header">
             <h5 class="modal-title">Prove To Be Human</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -18,6 +16,5 @@
                 </center>
             </form>
         </div>
-    </div>
-            </div></div>
+
 
