f5955bdbf805ba9d340c05855b79d301a6a5d25c
bestboom <certseek@gmail.com>
2019-01-29 12:23:47 +0500

    Update claim.php

--- content/claim.php
+++ content/claim.php
@@ -59,7 +59,9 @@
         </div>
     </div>
     <div class="modal fade" id="premium_modal" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+        <div class="modal-dialog" role="document">
         <?php include(template/modals/premium.php); ?>
+        </div>
     </div>
 
 </div>
