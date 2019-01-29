8f7cf725d7e8176df47496c6ec902ca5784b752a
bestboom <certseek@gmail.com>
2019-01-29 12:19:08 +0500

    Create premium.php

---
+++ template/modals/premium.php
@@ -0,0 +1,21 @@
+<div class="modal-dialog" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title">Prove To Be Human</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-new" style="padding: 60px">
+            <div id="output-msg"></div>
+            <form action="solve.php" method="post" id="logsubmit">
+                <div class="form-group">
+                    <p>Premium</p>
+                </div>
+                <center>
+                    <button id="verify" type="submit" class="button button-sm button-secondary button-nina">CLAIM</button>
+                </center>
+            </form>
+        </div>
+    </div>
+</div>
