3172742c1a440811f6f8132128188dec608dc471
bestboom <certseek@gmail.com>
2019-01-30 17:54:52 +0500

    Update home.php

--- content/home.php
+++ content/home.php
@@ -30,7 +30,7 @@
                 <li class="text-center"><span><i class="fa fa-discord circle-up "></i></span>
                   <p class="text-spacing-sm offset-0">DOSCORD</p>
                 </li>
-                <li class="text-center"><span><i class="fa fa-telegram circle-up " aria-hidden="true"></i></span>
+                <li class="text-center"><span><i class="fa fa-telegram-plane circle-up " aria-hidden="true"></i></span>
                   <p class="text-spacing-sm offset-0">TELEGRAM</p>
                 </li>
                 <li class="text-center"><span><i class="fa fa-reddit circle-up " aria-hidden="true"></i></span>
