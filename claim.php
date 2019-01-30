575b0b7041dcf8c3aaa4d30c08adaba674c9aab7
bestboom <certseek@gmail.com>
2019-01-30 22:56:06 +0500

    Update claim.php

--- claim.php
+++ claim.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 include ('template/header.php');
+require 'src/lib/solvemedialib.php';
+$privkey="FjmpDymsxjGHoE4kjLBWlxAwDbowmDUK";  // SolveMedia Key
+$hashkey="lt5d05u8AQOj9ZwpGKxE8U.ELJQPruBd";  // SolveMedia Key
 ?>
 <article>
     <?php include ('content/claim.php'); ?>
