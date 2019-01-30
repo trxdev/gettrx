120de43977e83372324c177a39ad058e8c316933
bestboom <certseek@gmail.com>
2019-01-30 14:36:15 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
-<?php
-session_start();
-include ('template/header.php');
-require ('src/lib/solvemedialib.php');
-error_reporting(E_ALL);
-
-
-$privkey="FjmpDymsxjGHoE4kjLBWlxAwDbowmDUK";  // SolveMedia Key
-$hashkey="lt5d05u8AQOj9ZwpGKxE8U.ELJQPruBd";  // SolveMedia Key
-
-$solvemedia_response = solvemedia_check_answer($privkey,
-					$_SERVER["REMOTE_ADDR"],
-					$_POST["adcopy_challenge"],
-					$_POST["adcopy_response"],
-					$hashkey);
-
-?>
-
-
-<center><?php echo solvemedia_get_html("xLxIw6ibOZWL52J97WjID7wkCMJzave8"); ?> </center><br>
-<article>
-	<?php include ('content/home.php'); ?>
-</article>
+<?php session_start();
+include ('template/header.php'); ?>
+<article><?php include ('content/home.php'); ?></article>
 <?php include ('template/footer.php'); ?>
