efcbf840aa22ee1e68ef929601823dc65633faea
bestboom <certseek@gmail.com>
2019-01-30 23:02:34 +0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,3 +1,5 @@
 <?php
 $c = '<iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=LOQBU2V7573MQ" style="overflow:hidden;width:728px;height:90px;" frameborder="0"></iframe>';
 $a = '<iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=D2SUKIIVOLPOL" style="overflow:hidden;width:300px;height:250px;" frameborder="0"></iframe>';
+$privkey="FjmpDymsxjGHoE4kjLBWlxAwDbowmDUK";  // SolveMedia Key
+$hashkey="lt5d05u8AQOj9ZwpGKxE8U.ELJQPruBd";  // SolveMedia Key
