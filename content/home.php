9f831406b16bc90c9bd9d9803b74ae62e65305b4
bestboom <certseek@gmail.com>
2019-01-30 17:47:39 +0500

    Update home.php

--- content/home.php
+++ content/home.php
@@ -2,8 +2,7 @@
     <div class="row">
         <div class="col-md-5">
             <div class="col-md-12" style="padding:45px;border: 1px solid #000;border-radius: 7px;background: #e9ecef;">
-
-                <h6 class="incenter">CLAIM FREE TRX<br>Every 10 Minutes</h6>
+                <h6 class="incenter" style="margin-top:5px;">CLAIM FREE TRX<br>Every 10 Minutes</h6>
                 <div class="incenter" style="margin:15px;"><a href="claim.php" class="button button-sm button-secondary button-nina"><span id="claim">CLAIM NOW</span></a></div>
                 <h5 class="incenter">BECOME PREMIUM MEMBER TO CLAIM MORE</h5>
                 <div class="incenter">
@@ -22,19 +21,19 @@
                     <br> You can claim free trons on this tron faucet. We give away 10,000 SUN (0.01 TRX) every 10 minutes. To claim free tron, you must follow rules and guidelines and should not do any activities that harm the good purpose fo this faucet.</p>
             
             <ul class="inline-list">
-                <li class="text-center"><span><i class="fa fa-twitter" aria-hidden="true"></i></span>
+                <li class="text-center"><span><i class="fa fa-twitter circle-up " aria-hidden="true"></i></span>
                   <p class="text-spacing-sm offset-0">TWITTWR</p>
                 </li>
-                <li class="text-center"><span><i class="fa fa-facebook" aria-hidden="true"></i></span>
+                <li class="text-center"><span><i class="fa fa-facebook circle-up " aria-hidden="true"></i></span>
                   <p class="text-spacing-sm offset-0">FACEBOOK</p>
                 </li>
-                <li class="text-center"><span><i class="fa fa-discord"></i></span>
+                <li class="text-center"><span><i class="fa fa-discord circle-up "></i></span>
                   <p class="text-spacing-sm offset-0">DOSCORD</p>
                 </li>
-                <li class="text-center"><span><i class="fa fa-telegram" aria-hidden="true"></i></span>
+                <li class="text-center"><span><i class="fa fa-telegram circle-up " aria-hidden="true"></i></span>
                   <p class="text-spacing-sm offset-0">TELEGRAM</p>
                 </li>
-                <li class="text-center"><span><i class="fa fa-reddit" aria-hidden="true"></i></span>
+                <li class="text-center"><span><i class="fa fa-reddit circle-up " aria-hidden="true"></i></span>
                   <p class="text-spacing-sm offset-0">REDDIT</p>
                 </li>
               </ul>
