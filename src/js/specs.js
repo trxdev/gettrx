35c75045a8cfaf2b4d1c2fd303f6e1d981c559f6
bestboom <certseek@gmail.com>
2019-01-30 14:31:28 +0500

    Update specs.js

--- src/js/specs.js
+++ src/js/specs.js
@@ -5,15 +5,19 @@ $(document).ready(function(){
         var inputvalue = $('#trx_input').val();
         var mainprice = '$' + parseFloat(price).toFixed(4);
         var pricechange = '(' + (parseFloat(result.data['changePercent24Hr']).toFixed(2)) + '%)';
-
+      
+        $('#cap').html('$' + ((result.data['marketCapUsd'])/1000000000).toFixed(2) + 'B');
         $('#rank').html(result.data['rank']);
+        $('#volumetoday').html('$' + ((result.data['volumeUsd24Hr'])/1000000).toFixed(2) + 'M');
+        $('#price').html('$' + parseFloat(price).toFixed(6));
+      
         $('title').html('Tron Price ' + mainprice + ' Live Tron Chart TRX Price');
         $('#mdescription').attr('content', 'Tron price today is' +  mainprice + 'with chnage of ' + pricechange + ' in last 24 hours. See Tron (TRX) price chart for real time tron pric.' );
         $('#name').html(result.data['id']);
         $('#price').html('$' + parseFloat(price).toFixed(6));
         $('#change').html('(' + (parseFloat(result.data['changePercent24Hr']).toFixed(2)) + '%)');
         
-        $('#cap').html('$' + ((result.data['marketCapUsd'])/1000000000).toFixed(2) + 'B');
+        
         $('#volumetoday').html('$' + ((result.data['volumeUsd24Hr'])/1000000).toFixed(2) + 'M');
         $('#supply').html(((result.data['supply'])/1000000000).toFixed(2) + 'B TRX' );
         $('#usd_price').val((inputvalue * price).toFixed(6));
