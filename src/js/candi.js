e97674b337e1494a30dbe09a20833a4a16cd1adc
bestboom <certseek@gmail.com>
2019-02-02 17:33:59 +0500

    Update candi.js

--- src/js/candi.js
+++ src/js/candi.js
@@ -1,5 +1,5 @@
 var TRON = {
-    CONTRACT_ADDRESS: "TPcLTD2RdLYtrhBFKqpVujupj9fTJqdmxH",
+    CONTRACT_ADDRESS: "TU3LAfY3XCYTE8E2bknHYuB7WXhKhnGzb3",
     //CONTRACT_ADDRESS: "TLijiFWfhK55BKCEZ2Up1yc35c5SiYkvDr",
     contractInstance: "",
     init: async function() {
