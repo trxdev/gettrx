525b0f15b58a7687db104fbe16783f17a9d04bd0
bestboom <certseek@gmail.com>
2019-02-04 01:00:40 +0500

    Update claim.php

--- claim.php
+++ claim.php
@@ -4,6 +4,7 @@ include ('template/header.php');
 require 'src/lib/solvemedialib.php';
 ?>
 <article>
-    <?php include ('content/claim.php'); ?>
+    <?php include ('content/claim.php'); 
+    echo $TOKEN_ID = getenv('TOKEN_ID'); ?>
 </article>
 <?php include ('template/footer.php'); ?>
