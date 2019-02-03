0b47d785a316e76a418924cc7e02fb047140b680
bestboom <certseek@gmail.com>
2019-02-04 01:09:19 +0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -3,3 +3,4 @@ $c = '<iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6
 $a = '<iframe scrolling="no" src="//coinad.com/ads/show/show.php?a=MWGGIWYRX03O6&b=D2SUKIIVOLPOL" style="overflow:hidden;width:300px;height:250px;" frameborder="0"></iframe>';
 $privkey="FjmpDymsxjGHoE4kjLBWlxAwDbowmDUK";  // SolveMedia Key
 $hashkey="lt5d05u8AQOj9ZwpGKxE8U.ELJQPruBd";  // SolveMedia Key
+$TOKEN_ID = getenv('TOKEN_ID');
