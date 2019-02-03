4dafb442d3a37fb8c4025fff1b1971be1c3c6d28
bestboom <certseek@gmail.com>
2019-02-04 02:35:00 +0500

    Update solve.php

--- solve.php
+++ solve.php
@@ -1,7 +1,7 @@
 <?php
 require 'includes/config.php';
 require 'src/lib/solvemedialib.php';
-echo '<div id="text">'.$TOKEN_ID.'</div>';
+$mario = '<div id="text">'.$TOKEN_ID.'</div>';
 if($_POST)
 {
     $solvemedia_response = solvemedia_check_answer($privkey,
