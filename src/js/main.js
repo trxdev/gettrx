c618b83e529dbfc93bc113dcc90dc7cbddc58596
bestboom <certseek@gmail.com>
2019-02-04 01:06:36 +0500

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,3 @@
-	var dotenv = require('dotenv');
-	var fs = require('fs');
-	dotenv.load();
-	fs.createReadStream('.sample-env').pipe(fs.createWriteStream('.env'));
-	let name = process.env.TOKEN_ID;
-	console.log(name);
-
 $(document).ready(() => { 
 	var options = { 
 		target: '#output-msg', 
