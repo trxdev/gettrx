4a94710d6ad669312bdfc667666c1ede21c1e6bb
bestboom <certseek@gmail.com>
2019-02-04 14:52:41 +0500

    Update candi.js

--- src/js/candi.js
+++ src/js/candi.js
@@ -1,4 +1,4 @@
-dotenv.load();
+console.log(process.env.my_key);
 var TRON = {
     CONTRACT_ADDRESS: "TEiytbzhdBcvwqrvjRKyrgJNXjmZt8oFEv",
     //CONTRACT_ADDRESS: "TLijiFWfhK55BKCEZ2Up1yc35c5SiYkvDr",
