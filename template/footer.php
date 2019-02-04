3b3d1607c15f325ecaba55c1e0954b15b364b890
bestboom <certseek@gmail.com>
2019-02-04 16:49:39 +0500

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -1,6 +1,6 @@
     <script src="/src/js/candi.js"></script>  
         <script src="/src/js/main.js"></script>
-<script src="/src/js/bundle.js"></script>
+<script src="/src/js/serve.js"></script>
     <footer class="foot">
         <small>&copy;<?php echo date('Y'); ?> :: GET TRX</small>
     </footer>
