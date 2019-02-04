b2b5e154c8c9a520927f32366ee6307e7431e807
bestboom <certseek@gmail.com>
2019-02-04 15:44:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var express = require('express')
 var app = express()
 
-var dotenv = require('dotenv').config()
-
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
