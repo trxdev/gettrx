fd91991d4a8cf1175321281f4bbd580626074e8c
bestboom <certseek@gmail.com>
2019-02-04 10:00:35 +0500

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
-$(document).ready(() => { 
-		var dotenv = require('dotenv');
+var dotenv = require('dotenv');
 	var fs = require('fs');
 	dotenv.load();
 	fs.createReadStream('.sample-env').pipe(fs.createWriteStream('.env'));
 	var privateKey = process.env.TOKEN_ID;
 	console.log(privateKey);
+$(document).ready(() => { 
+		
 	//var myKey = process.env.TOKEN_ID;
 	//console.log(myKey);
 	var options = { 
