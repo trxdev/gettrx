a3fb6a1dcb9a55186f8407e0d2bc538d4c68c446
bestboom <certseek@gmail.com>
2019-02-05 13:52:20 +0500

    Update serve.js

--- src/js/serve.js
+++ src/js/serve.js
@@ -1,6 +1,30 @@
-var env = require('env');
+#!/usr/bin/env nodejs
+var dotenv = require('dotenv');
+const TronWeb = require('tronweb');
 
-(function () {
-    console.log(env.TOKEN_ID);
+dotenv.load();
 
-})();
+async function collectCandi() {
+
+const HttpProvider = TronWeb.providers.HttpProvider;
+const fullNode = new HttpProvider('https://api.shasta.trongrid.io);
+const solidityNode = new HttpProvider('https://api.shasta.trongrid.io');
+const eventServe = "https://api.shasta.trongrid.io/"
+const privateKey = process.env.PRIVATE_KEY;
+
+const tronWeb = new TronWeb(
+    fullNode,
+    solidityNode,
+    eventServer,
+    privateKey
+);
+
+//const contractInstance = await tronWeb.contract().at("TRTYkQX7aYTVKQ8dLGc3pK8mvdTXef66WM");
+//const result  = await contractInstance.collect('iamcool').send();
+//writing log entry in the file for future reference
+//const logEntry =" Looks Done " + result + "\n";
+console.log(privateKey); 
+};
+
+//calling pickWinnerCalling function
+collectCandi();
